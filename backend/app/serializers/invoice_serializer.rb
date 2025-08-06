544c577427e735ced70360fda255f84fbc7e8597
Carlos morin <jose.morin.riojas@gmail.com>
2025-08-06 10:17:56 -0600

    backend: change format index response

--- backend/app/serializers/invoice_serializer.rb
+++ backend/app/serializers/invoice_serializer.rb
@@ -1,6 +1,10 @@
 class InvoiceSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :receiver_name, :issued_at, :uuid, :subtotal,
+  attributes :id, :receiver_name, :issued_at, :uuid, :subtotal,
              :total, :status
+
+  def issued_at
+    object.issued_at.strftime("%d/%m/%Y")
+  end
 end
